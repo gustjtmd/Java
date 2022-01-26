183e0748996fa6753c2fa08a71d652fee30f0c1d
seoseunghyeon <gustjtmd@gmail.com>
2022-01-26 20:17:41 +0900

    개인공부 열혈 Java 프로그래밍

---
+++ src/review/ch11/AutoCallToString.java
@@ -0,0 +1,24 @@
+package review.ch11;
+class Box{
+    private String conts;
+
+    Box(String cont){
+        this.conts = cont;
+    }
+    public String toString(){
+        return conts;   //문자열 반환
+    }
+}
+
+public class AutoCallToString {
+    public static void main(String[] args) {
+        StringBuilder stb = new StringBuilder("12");
+        stb.append(34);
+        System.out.println(stb.toString());
+        System.out.println(stb);
+
+        Box box = new Box("Camera");
+        System.out.println(box.toString());
+        System.out.println(box);
+    }
+}
