a2fa48016c55ff1258d2866848b968a471213bfb
seoseunghyeon <gustjtmd@gmail.com>
2022-02-02 23:28:23 +0900

    개인공부 열혈 Java 프로그래밍

---
+++ src/review/ch19/StringEquality.java
@@ -0,0 +1,19 @@
+package review.ch19;
+public class StringEquality {
+    public static void main(String[] args) {
+        String str1 = new String("So Simple");
+        String str2 = new String("So Simple");
+
+        // 참조 대상을 비교하는 if ~ else 문
+        if(str1 == str2)
+            System.out.println("str1 str2 참조 대상 동일하다.");
+        else
+            System.out.println("str1 str2 참조 대상 다르다.");
+
+        //두 인스턴스를 비교하는 if ~ else 문
+        if(str1.equals(str2))
+            System.out.println("str1 str2 참조 대상 동일하다.");
+        else
+            System.out.println("str1 str2 참조 대상 다르다");
+    }
+}
