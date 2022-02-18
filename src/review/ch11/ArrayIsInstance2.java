3e5ad4a4b13f49dc4364fdf5975a282016bacbbd
seoseunghyeon <gustjtmd@gmail.com>
2022-02-18 20:49:24 +0900

    개인공부 열혈 Java 프로그래밍

--- src/review/ch11/ArrayIsInstance2.java
+++ src/review/ch11/ArrayIsInstance2.java
@@ -15,4 +15,29 @@ public class ArrayIsInstance2 {
         Box2[] ar = new Box2[5];  //길이가 5인 Box형 1차원 배열의 생성
         System.out.println("length : "+ar.length);  //length: 5
     }
+
+    public static class ArrayTest2 {
+
+        public static int minValue(int[] arr){
+            int result = arr[0];
+            for(int i = 0; i < arr.length; i++)
+                if(result > arr[i])
+                    result = arr[i];
+            return result;
+        }
+        public static int maxValue(int[] arr){
+            int result = 0;
+            for(int i : arr)
+                if(result < i)
+                    result = i;
+            return result;
+        }
+
+        public static void main(String[] args) {
+            int[] ar = {10,20,30,40,50};
+            System.out.println("최소값 : "+minValue(ar));
+            System.out.println("최대값 : "+maxValue(ar));
+
+        }
+    }
 }
